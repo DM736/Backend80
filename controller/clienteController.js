89b0b2963108af3941e1a7c0012e998644c77d71
Diego Rodriguez <mundie192@gmail.com>
2024-06-26 18:41:42 -0500

    modulo de clientes tipo CRUD completo

--- controller/clienteController.js
+++ controller/clienteController.js
@@ -3,9 +3,7 @@ const Cliente = require ('../models/Cliente');
 exports.agregarClientes = async(req, res) => {
 
     try {
-
-    let clientes;
-    clientes = new Cliente(req.body);
+    let clientes = new Cliente(req.body);
     await clientes.save();
     res.send(clientes);
 
@@ -14,4 +12,84 @@ exports.agregarClientes = async(req, res) => {
     res.status(500).send('Hubo un error al agregar un cliente');
     }
 
+}
+//funcion para mostar las Cliente hechas 
+exports.mostrarClientes = async(req, res) =>{
+    try {
+        const cliente= await Cliente.find();
+        res.json(cliente);
+      } catch (error) {
+        console.log(error)
+        res.status(502).send("Error al mostrar el cliente.");
+      }
+};
+//funcion para mostrar una cliente en especifico
+exports.buscarClientes = async(req,res)=>{
+    try {
+        const cliente = await Cliente.findById(req.params.id);
+        if(!cliente){
+            return res.status(404).json({ msg: "cliente no encontada"});
+        }
+        res.send(cliente); 
+    } catch (error) {
+        res.status(500).send("Error al consultar la cliente");
+    }
+};
+
+// funcion para eliminar Cliente
+exports.eliminarClientes = async(req, res) =>{
+    try {
+        let cliente = await Cliente.findById(req.params.id);
+        if(!cliente){
+            res.status(404).json({msg: "No hay registro de la cliente"}) 
+        }else{
+            await Cliente.findOneAndDelete({_id: req.params.id});
+            res.json({msg: "El cliente ha sido eliminado"});
+        }
+    } catch (error) {
+        res.status(500).send("ERROR, no se pudo eliminar el cliente");
+    }
+}
+//funcion para actualizar el cliente
+exports.actualizarClientes = async(req, res) =>{
+    try {
+        const {nombres, apellidos, cedula, correo, numeroContacto, nit,direccion} = req.body
+        let cliente = await Cliente.findById(req.params.id)
+        if(!cliente){
+            res.status(404).json({msg: "No hay registro de la cliente"});
+            return
+        }
+            cliente.nombres = nombres;
+            cliente.apellidos = apellidos;
+            cliente.cedula = cedula;
+            cliente.correo = correo;
+            cliente.numeroContacto = numeroContacto;
+            cliente.nit = nit;
+            cliente.direccion = direccion;
+            cliente = await Cliente.findOneAndUpdate({_id: req.params.id}, cliente,{new: true});
+            res.json(cliente);
+    } catch (error) {
+        res.status(500).send("hubo un error al actualizar el cliente");
+    }
+}
+//funcion para actualizar el cliente con metodo PUT
+exports.actualizarClientes = async(req,res)=>{
+    try {
+        const cliente = await Cliente.findOneAndUpdate({_id: req.params.id},req.body);
+        if(!cliente) res.status(404).send("cliente no encontrado");
+        else
+            res.json(cliente);
+    } catch (error) {
+        res.status(500).send("hubo un error al actualizar el cliente");
+    }
+}
+//funcion para actualizar cliente con metodo PATCH
+exports.cambioClientes = async(req,res)=>{
+    try {
+        const cliente = await Cliente.findByIdAndUpdate(req.params.id, req.body, {new: true});
+        if(!cliente) res.status(404).send("cliente no encontrado");
+            res.json(cliente);
+    } catch (error) {
+        res.status(500).send("hubo un error al actualizar el cliente");
+    }
 }
--- routes/cliente.js
+++ routes/cliente.js
@@ -3,5 +3,10 @@ const router = express.Router();
 const clienteController = require('../controller/clienteController');
 
 router.post('/', clienteController.agregarClientes);
+router.get('/', clienteController.mostrarClientes);
+router.get('/:id', clienteController.buscarClientes);
+router.delete('/:id', clienteController.eliminarClientes);
+//router1.put('/:id', clienteController.actualizarClientes);
+router.patch('/:id', clienteController.cambioClientes);
 
 module.exports = router 
